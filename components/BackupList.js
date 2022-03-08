a005a5d3eb8c4ecffe15fb7c2e5010b8a4370b36
SamirMaoude <samirmaoude7@gmail.com>
2022-03-08 19:25:31 +0100

    backup storing

--- components/BackupList.js
+++ components/BackupList.js
@@ -1,54 +1,81 @@
 import React from 'react'
 import { TextInput } from 'react-native'
-import {
-    View,
-    StyleSheet,
-    Text,
-    FlatList,
-    TouchableOpacity,
-    KeyboardAvoidingView,
-    TouchableWithoutFeedback,
-    Keyboard} from 'react-native'
+import {View, StyleSheet, Text, FlatList, TouchableOpacity, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard} from 'react-native'
 import Backup from './partials/Backup'
+import {connect} from 'react-redux'
 import {default as MaterialIcons} from 'react-native-vector-icons/MaterialIcons'
-
+import { authentication, db } from "../firebase/firebase-config"
+import { doc, setDoc, addDoc, collection, getDocs, deleteDoc } from "firebase/firestore/lite"; 
 
 class BackupList extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            backup: null,
+            backup: "",
             backupItems: [
-                {id: 'a', name: 'Sauvegarde 1'},
-                {id: 'b', name: 'Sauvegarde 2'}
-        
             ]
         }
+
+        this.backupRef = collection(db, 'backups')
     }
-    handleAddBackup = () => {
-        this.setState({
-            backupItems: [...this.state.backupItems, {name:this.state.backup, id:this.state.backup}],
-            backup: null,
+
+    componentDidMount(){
+        this.getBackups()
+    }
+
+    getBackups = async () => {
+        const backupsSnapshot = await getDocs(this.backupRef);
+        let backupsList = []
+        for(let doc of backupsSnapshot.docs){
+            let backup = {
+                id: doc.id,
+                ...doc.data()
+            }
+
+            backupsList.push(backup)
+        }
+
+        backupsList.sort((a, b) => {
+            var d1 = new Date(a.createdAt), d2 = new Date(b.createdAt)
+
+            return d1 < d2
         })
+
+        this.setState({
+            backupItems: backupsList
+        })
+
+    }
+
+    handleAddBackup = async () => {
+        
+        if(this.state.backup.length===0 || this.state.backup.trim().length===0) return;
+
+        
+        const backup = {
+            userId: authentication.currentUser.uid,
+            name: this.state.backup,
+            createdAt: Date.now().toString(),
+            rabbits: this.props.rabbitsList,
+            reproductions: this.props.reproductionsList,
+            vaccinations: this.props.vaccinationsList
+        }
+
+        await addDoc(this.backupRef, backup, { merge: true }).then((docRef)=>{
+        
+            this.getBackups()
+    
+        }).catch((e)=>{
+              console.log(e)
+        })
+
     }
 
     
     render(){
         return(
             <View style={styles.container}>
-                <View style={styles.backupsWrapper}>
-                    {/* <Text style={styles.sectionTitle}>Sauvegardes</Text> */}
-                    <FlatList
-                        data = {this.state.backupItems}
-                        extraData={this.state.backupItems}
-                        keyExtractor = {(item) => item.id.toString()}
-                        renderItem = {({item}) => <Backup text={item.name} />}
-                        onEndReachedThreshold={1}
-                        contentContainerStyle={styles.items}
-                    />
-                </View>
-
                 <KeyboardAvoidingView 
                     behavior={Platform.OS === "ios" ? "padding" : "height"}
                     style={styles.writeTaskWrapper}
@@ -71,7 +98,17 @@ class BackupList extends React.Component{
                     </TouchableOpacity>
                     
                 </KeyboardAvoidingView>
-
+                <View style={styles.backupsWrapper}>
+                    {/* <Text style={styles.sectionTitle}>Sauvegardes</Text> */}
+                    <FlatList
+                        data = {this.state.backupItems}
+                        extraData={this.state.backupItems}
+                        keyExtractor = {(item) => item.id.toString()}
+                        renderItem = {({item}) => <Backup text={item.name} />}
+                        onEndReachedThreshold={1}
+                        contentContainerStyle={styles.items}
+                    />
+                </View>
                 
             </View>
         )
@@ -81,11 +118,13 @@ class BackupList extends React.Component{
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
         backgroundColor: '#E8EAED'
     },
 
     backupsWrapper: {
-        paddingHorizontal: 20,
+        paddingHorizontal: 10,
+        paddingTop: 30
     },
     sectionTitle: {
         fontSize: 24,
@@ -114,14 +153,21 @@ const styles = StyleSheet.create({
         borderWidth: 1,
     },
     writeTaskWrapper: {
-        position: 'absolute',
-        bottom: 60,
+        top: 20,
         width: '100%',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     addText: {},
 })
 
-export default BackupList
+const mapStateToProps = (state) => {
+    return {
+        rabbitsList: state.rabbitManager.rabbitsList,
+        reproductionsList: state.reproductionManager.reproductionsList,
+        vaccinationsList: state.vaccinationManager.vaccinationsList
+    }
+}
+
+export default connect(mapStateToProps)(BackupList)
